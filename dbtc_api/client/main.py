ab402abc983a50d61e54b97d634278f9655950ce
Doug Guthrie <douglas.p.guthrie@gmail.com>
2022-05-05 14:27:17 -0600

    Point cloud property to new consolidated class

--- dbtc_api/client/main.py
+++ dbtc_api/client/main.py
@@ -1,9 +1,9 @@
 # first party
-from dbtc_api.client.cloud import _CloudClientV2
+from dbtc_api.client.cloud import _CloudClient
 from dbtc_api.client.metadata import _MetadataClient
 
 
 class dbtCloudClient:
     def __init__(self, **kwargs):
-        self.cloud = _CloudClientV2(**kwargs)
+        self.cloud = _CloudClient(**kwargs)
         self.metadata = _MetadataClient(**kwargs)
