0f8602fb26cc22f60b1421ced907563a3b7f1e73
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-03-04 11:47:25 -0700

    Add model to validate webhook payload

---
+++ dbtc/models/webhooks.py
@@ -0,0 +1,24 @@
+# stdlib
+from typing import List, Literal, Optional
+
+# third party
+from pydantic import BaseModel, conlist
+
+
+class Webhook(BaseModel):
+
+    # Required
+    active: bool
+    client_url: str
+    event_types: conlist(  # type: ignore
+        Literal['job.run.started', 'job.run.completed', 'job.run.errored'],
+        unique_items=True,
+    )
+    name: str
+
+    # Optional
+    deactivate_reason: Optional[str]
+    description: Optional[str]
+    id: Optional[str]
+    job_ids: List[int]
+    test: Optional[str]
