aa7a94e0b1258357c5f4cec7ab8c83f5931ccf92
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-07-30 15:57:14 -0600

    Add default method to update env var method

--- dbtc/client/admin.py
+++ dbtc/client/admin.py
@@ -1769,7 +1769,7 @@ class _AdminClient(_Client):
 
     @v3
     def update_environment_variables(
-        self, account_id: int, project_id: int, payload: Dict
+        self, account_id: int, project_id: int, payload: Dict, method: str = 'put'
     ):
         """Update an environment variable
 
@@ -1778,9 +1778,15 @@ class _AdminClient(_Client):
             project_id (int): Numeric ID of the project
             payload (dict): Dictionary representing the environment to update
         """
+        is_valid_method = method.lower() in ['put', 'post']
+        if not is_valid_method:
+            raise ValueError(
+                'Invalid method specified.  Valid methods include put or post.'
+            )
+
         return self._simple_request(
             f'accounts/{account_id}/projects/{project_id}/environment-variables/bulk',  # noqa: E501
-            method='post',
+            method=method,
             json=payload,
         )
 
